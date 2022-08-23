2fb4b365868418a41caac4dbb2a3fc08b6b5bd09
Roman <brysommer1@gmail.com>
2022-08-23 13:25:00 +0300

    lvl1

---
+++ script/main.js
@@ -0,0 +1,46 @@
+const getPlanet = async (number) => {
+    const result = await axios.get('https://swapi.dev/api/planets/' + number);
+    console.log(result.data);
+    renderPlanet(result);
+
+};
+
+
+const el1 = document.querySelector('select[name="planetname"]');
+const el2 = document.querySelector('input[name="planetnumber"]');
+const el3 = document.querySelector('input[name="surch"]');
+const el4 = document.querySelector('.info')
+
+const renderPlanet = (result) => {
+    el4.innerHTML = '';
+    let planetname = `<h3>Name: ${result.data.name} </h3>`;
+    let diameter = `<li>diameter: ${result.data.diameter} </li>`;
+    let rotation_period = `<li>rotation period: ${result.data.rotation_period} </li>`;
+    let climate = `<li>climate: ${result.data.climate} </li>`;
+    let gravity = `<li>gravity: ${result.data.gravity} </li>`;
+    let population = `<li>population: ${result.data.population} </li>`;
+    let surface_water = `<li>surface_water: ${result.data.surface_water} </li>`;
+    let terrain = `<li>terrain: ${result.data.terrain} </li>`;
+    let orbital_period = `<li>orbital_period: ${result.data.orbital_period} </li>`;
+
+    el4.insertAdjacentHTML('beforeend', planetname);
+    el4.insertAdjacentHTML('beforeend', diameter);
+    el4.insertAdjacentHTML('beforeend', climate);
+    el4.insertAdjacentHTML('beforeend', gravity);
+    el4.insertAdjacentHTML('beforeend', population);
+    el4.insertAdjacentHTML('beforeend', surface_water);
+    el4.insertAdjacentHTML('beforeend', terrain);
+    el4.insertAdjacentHTML('beforeend', orbital_period);
+    el4.insertAdjacentHTML('beforeend', rotation_period);
+}
+
+el1.addEventListener('change', (ev) => {
+    let number = ev.target.value;
+    getPlanet(number);    
+});
+
+el3.addEventListener('click', (click) => {
+    let number = el2.value;
+    getPlanet(number);
+});
+
